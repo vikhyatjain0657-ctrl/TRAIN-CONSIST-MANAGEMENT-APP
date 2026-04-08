06c45a8b58ebf1146f4dcb513456a47283e74eb3
vikhyatjain0657-ctrl <vikhyatjain0657@gmail.com>
2026-04-08 22:42:02 +0530

    UC1: Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> bogies = new ArrayList<>();
+
+
+        System.out.println("Train consist initialized.");
+        System.out.println("Initial number of bogies: " + bogies.size());
+
+    }
 }
