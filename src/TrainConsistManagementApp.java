1b6fd8e84000b1bcb4c8d1b094cd81b64cea4309
vikhyatjain0657-ctrl <vikhyatjain0657@gmail.com>
2026-04-09 13:09:38 +0530

    UC16

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -68,6 +68,27 @@ class GoodsBogie {
 
 public class TrainConsistManagementApp {
 
+    public static int[] bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+        }
+
+        return arr;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("=== Train Consist Management App ===");
